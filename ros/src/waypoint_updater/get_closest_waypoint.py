f1025e6b41e324ff44f1480de220edf751d83dbc
Thomas Milas <itpro41994@gmail.com>
2018-12-07 09:00:19 -0800

    disabled get closest waypoint

--- ros/src/waypoint_updater/get_closest_waypoint.py
+++ ros/src/waypoint_updater/get_closest_waypoint.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 import rospy
-from waypoint_updater.srv import GetClosestWaypointRequest
+# from waypoint_updater.srv import *
+import os
 
 def handle(req):
     pass
 
 
 def init():
+    print os.getcwd()
     rospy.init_node('get_closest_waypoint_service')
-    rospy.roswarn('starting get closest waypoint service')
+    rospy.logwarn('starting get closest waypoint service')
     # service = rospy.Service('get_closest_waypoint_service', GetClosestWaypoint, handle)
 
     rospy.spin()
